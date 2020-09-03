742d622e0221cb427a38af8fc55533bbd13e85af
vinodlowzz <vinodvld10@gmail.com>
2020-09-03 14:48:51 +0530

    final final out

---
+++ app.js
@@ -0,0 +1,28 @@
+$(window).on('load',function() {
+     // Animate loader off screen
+     jQuery(".se-pre-con").fadeOut("slow");
+   });
+   
+   $(document).ready(function () {
+    // Smooth scroll for the navigation and links with .scrollto classes
+    $("a").on("click", function () {
+      if (
+        location.pathname.replace(/^\//, "") ==
+          this.pathname.replace(/^\//, "") &&
+        location.hostname == this.hostname
+      ) {
+        var target = $(this.hash);
+        if (target.length) {
+          $("html, body").animate(
+            {
+              scrollTop: target.offset().top - 90,
+            },
+            1500,
+            "easeInOutExpo"
+          );
+  
+          return false;
+        }
+      }
+    });
+  });
