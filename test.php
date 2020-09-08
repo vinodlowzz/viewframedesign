013af0831bb1c0b6315fd2113fc6dcc771171786
vinodlowzz <vinodvld10@gmail.com>
2020-09-08 10:52:12 +0530

    php mailer test

--- test.php
+++ test.php
@@ -1,36 +1,57 @@
-
+<!DOCTYPE html>
+ <html lang="en" dir="ltr">
+   <head>
+     <meta charset="utf-8">
+     <title></title>
+   </head>
+   <body>
+     Hey There
+     <form  action="test.php" method="post">
+      <input type="submit" name="submit" value="">
+     </form>
+       
+   </body>
+ </html>
 <?php
 
+    
 use PHPMailer\PHPMailer\PHPMailer;
 use PHPMailer\PHPMailer\Exception;
 
+require 'PHPMailer/src/Exception.php';
 require 'PHPMailer/src/PHPMailer.php';
 require 'PHPMailer/src/SMTP.php';
-require 'PHPMailer/src/Exception.php';
 
+        
+if(isset($_POST['submit']))
+{
+    $mail = new PHPMailer(true);
 
-  $mail = new PHPMailer(true);
-  try {
-   
-    $mail = new PHPMailer;
-    $mail->Host='smtp.gmail.com';
-    $mail->Port=587;
-    $mail->SMTPAuth=true;
-    $mail->SMTPSecure='tls';
-    $mail->Username='enquiry@icatchermedia.com';
-    $mail->Password='mails@icatcher';
+    try {                    // Enable verbose debug output
+         $mail->Host='smtp.gmail.com';
+         $mail->Port=587;
+         $mail->SMTPAuth=true;
+         $mail->SMTPSecure='tls';
+         $mail->Username='info.viewframe@gmail.com';
+         $mail->Password='vfdesign2020';
 
+        $mail->setFrom('info.viewframe@gmail.com','View Frame Design',0);
+        $mail->addAddress("sharookhanb@gmail.com");
+        $mail->addReplyTo('info.viewframe@gmail.com','view Frame Design');
+    
+        // Content
+        $mail->isHTML(true);                                  // Set email format to HTML
+        $mail->Subject = 'Here is the subject';
+        $mail->Body    = 'This is the HTML message body <b>in bold!</b>';
+        $mail->AltBody = 'This is the body in plain text for non-HTML mail clients';
+    
+        $mail->send();
+        echo 'Message has been sent';
+    } catch (Exception $e) {
+        echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
+    }
+}
+        
+    
 
-
-// Content
-$mail->isHTML(true);                                  // Set email format to HTML
-$mail->Subject = 'Here is the subject';
-$mail->Body    = 'This is the HTML message body <b>in bold!</b>';
-$mail->AltBody = 'This is the body in plain text for non-HTML mail clients';
-
-$mail->send();
-    echo '<script>alert("Thank you, We will get back to you soon..!");</script>';
-  } catch (Exception $e) {
-    echo $e;
-  }
 ?>
